bc8a3ade25e2d006fde06ac9af1440fd85563bc3
Hugo Lapa <67755960+H-Lapa@users.noreply.github.com>
2021-11-13 20:55:09 +0000

    Bot Setup

---
+++ bot.py
@@ -0,0 +1,11 @@
+import discord
+import discord.ext import commands
+
+//sets bots prefix which commands the bot
+client  = commands.Bot(command_prefix = '!')
+
+
+
+
+//runs the discord bot
+client.run('YOUR DISCORD BOT TOKEN HERE') //Token is necessary for bot control
