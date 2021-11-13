5e5e6f1467870fd3ea036c4bb98d33957e2528c4
Hugo Lapa <67755960+H-Lapa@users.noreply.github.com>
2021-11-13 20:57:02 +0000

    Bot ready Func

--- bot.py
+++ bot.py
@@ -1,11 +1,14 @@
 import discord
 import discord.ext import commands
 
-//sets bots prefix which commands the bot
+#sets bots prefix which commands the bot
 client  = commands.Bot(command_prefix = '!')
 
+#prepares bot and if ready prints message
+@client.event
+async def on_ready():
+    print('Bot is ready to start!')
 
 
-
-//runs the discord bot
-client.run('YOUR DISCORD BOT TOKEN HERE') //Token is necessary for bot control
+#runs the discord bot
+client.run('YOUR DISCORD BOT TOKEN HERE') #Token is necessary for bot control
