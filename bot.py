b3b7f3a4e60c26ec05707f6d469b8410932cfa36
Hugo Lapa <67755960+H-Lapa@users.noreply.github.com>
2021-11-16 17:12:58 +0000

    joke command

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import discord
 import discord.ext import commands
+import random
 
 #sets bots prefix which commands the bot
 client  = commands.Bot(command_prefix = '!')
@@ -23,6 +24,13 @@ async def on_member_remove(member):
 @client.command()
 async def hello(ctx):
     await ctx.send(f"Hey, hope you're well {ctx.author.mention}")
+    
+
+#joke command tells joke to user from array    
+@client.command()
+async def joke(ctx):
+    jokes = ['What’s the best thing about Switzerland? I don’t know, but the flag is a big plus.', 'Did you hear about the first restaurant to open on the moon? It had great food, but no atmosphere.', 'Do you want to hear a construction joke? Sorry, I’m still working on it.', 'What does a nosey pepper do? It gets jalapeño business. ']
+    await ctx.send(f'{random.choice(jokes)}')
 
 
 #runs the discord bot
